0676c10240519613d96b0fcf30a24157ae085cab
Jesús Bossa <x@jesusbossa.dev>
2020-06-30 22:15:21 -0500

    [WIP] Color borders

--- src/components/dashCard/dashCard.js
+++ src/components/dashCard/dashCard.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import {View, Image, TouchableWithoutFeedback} from 'react-native';
 import styles from './styles';
+import {BoxShadow} from 'react-native-shadow';
 
 export default ({image, handlePress}) => {
   return (
-    <TouchableWithoutFeedback onPress={handlePress}>
-      <View style={styles.card}>
-        <Image style={styles.cover} source={{uri: image}} />
-      </View>
-    </TouchableWithoutFeedback>
+    <BoxShadow setting={shadowOpt}>
+      <TouchableWithoutFeedback onPress={handlePress} style={{position: 'relative'}}>
+        <View style={styles.card}>
+          <Image style={styles.cover} source={{uri: image}} />
+        </View>
+      </TouchableWithoutFeedback>
+    </BoxShadow>
   );
+};
+
+const shadowOpt = {
+  width: 180,
+  height: 150,
+  color: '#2ea44f',
+  margin: 10,
+  border: 2,
+  radius: 3,
+  opacity: 0.2,
+  x: 3,
+  y: 3,
+  style: {margin: 10, borderRadius: 100},
 };
--- src/components/dashCard/styles.js
+++ src/components/dashCard/styles.js
@@ -1,7 +1,7 @@
 import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
-  card: {marginTop: 10, marginRight: 5, marginLeft: 20, marginBottom: 10, borderRadius: 20, elevation: 4},
+  card: {},
   title: {
     color: '#ffffff',
   },
